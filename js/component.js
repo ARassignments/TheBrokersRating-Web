a4b236d2e7ccf9546872447312d03c68cdf58a6f
AR Assignments <49037725+ARassignments@users.noreply.github.com>
2025-04-21 22:06:14 +0500

    upwork

--- js/component.js
+++ js/component.js
@@ -1,20 +1,4 @@
-barba.init({
-  transitions: [
-    {
-      name: "opacity-transition",
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0,
-        });
-      },
-      enter(data) {
-        return gsap.from(data.next.container, {
-          opacity: 0,
-        });
-      },
-    },
-  ],
-});
+
 
 if (document.querySelector("header")) {
   document.querySelector("header").innerHTML = `
